50d90b3f1742e6c8ed37371a9259ede6e6650d1b
affan0101 <ahmadaffan284@gmail.com>
2026-07-20 22:10:03 +0530

    create API to get Available Menu Items

---
+++ backend/src/main/java/com/quickbite/backend/dto/Response/ProductResponse.java
@@ -0,0 +1,24 @@
+package com.quickbite.backend.dto.Response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductResponse {
+
+    private  Long id;
+
+    private String name;
+
+    private double price;
+
+    private String description;
+
+    private String status;
+}
